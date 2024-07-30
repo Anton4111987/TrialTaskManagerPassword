f8b9eae24507b29b66209de027e66d1f71135c3f
Anton <laant@mail.ru>
2024-07-30 21:34:55 +0500

    Ок

--- TrialTaskManagerPassword/Data/AppDBContext.cs
+++ TrialTaskManagerPassword/Data/AppDBContext.cs
@@ -5,12 +5,12 @@ namespace TrialTaskManagerPassword.Data
 {
     public class AppDBContext : DbContext
     {
+
         public DbSet<Record> Records { get; set; }
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
         {
            //  Database.EnsureCreated();
         }
-
 
     }
 }
